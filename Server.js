b52e56d41fab2f0a66aa23cdfc50cf922651215a
Yuvraj Singh <yuvrajsingh21114@gmail.com>
2026-06-15 15:24:13 +0530

    added uploads for frontend

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app= express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/api/uploads',express.static('uploads'));
 
 app.listen(3000,()=>{
     console.log("Server running at port 3000");
